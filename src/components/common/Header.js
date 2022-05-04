a0066315d00999439cef5fa644c90551fe9a23f9
chahyunwoo <chahyunwoobi@gmail.com>
2022-05-04 21:01:28 +0900

    반응형 헤더 버튼 생성

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header({ type }) {
-	const active = { color: 'orange'}
+	const active = { color: 'orange' };
+
+	// const [toggle, setToggle] = useState(false);
+
+	// const clickedToggle = () => {
+	// 	setToggle((prev) => !prev)
+	// 	console.log(toggle)
+	// }
 
 	return (
 		<header className={type}>
@@ -13,14 +20,44 @@ function Header({ type }) {
 					</NavLink>
 				</h1>
 
-        <ul id="gnb">
-          <li><NavLink activeStyle={active} to='/rooms'>ROOMS</NavLink></li>
-          <li><NavLink activeStyle={active} to='/gallery'>GALLERY</NavLink></li>
-          <li><NavLink activeStyle={active} to='/youtube'>YOUTUBE</NavLink></li>
-          <li><NavLink activeStyle={active} to='/faq'>FAQ</NavLink></li>
-          <li><NavLink activeStyle={active} to='/location'>LOCATION</NavLink></li>
-          <li><NavLink activeStyle={active} to='/join'>JOIN</NavLink></li>
-        </ul>
+				<ul id='gnb'>
+					<li>
+						<NavLink activeStyle={active} to='/rooms'>
+							ROOMS
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/gallery'>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/youtube'>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/faq'>
+							FAQ
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/location'>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/join'>
+							JOIN
+						</NavLink>
+					</li>
+				</ul>
+
+				<button className='menuMo' /*onClick={clickedToggle}*/>
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
 			</div>
 		</header>
 	);
