124c7038f12b96f7ec761ecb7aa0b6b6fc10bf82
chahyunwoo <chahyunwoobi@gmail.com>
2022-05-06 23:39:27 +0900

    유튜브 페이지 작업 중

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,10 +1,48 @@
-import React from 'react';
 import Layout from '../common/Layout';
+// import Popup from '../common/Popup'
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Youtube() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyAdo3TEXjvTi-2C_p9Z8zgbQD3uCz_JnAs';
+		const playlistId = 'PLdHIHMgBSgjLGDg3SJwV_MdhGnap-xuXg';
+		const num = 6;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
+
+		axios.get(url).then((json) => {
+			setVideos(json.data.items);
+		});
+	}, []);
+
 	return (
 		<Layout name={'youtube'}>
-			<p>YOUTUBE</p>
+			<div className='videosWrap'>
+				{videos.map((video, index) => {
+					const idx = (index + 1).toString();
+
+					return (
+						<article key={index}>
+							<h2>{`salt aewol #${
+								idx.length < 2 ? idx.padStart(2, '0') : idx
+							}`}</h2>
+							<div className='pic'>
+								<img
+									src={video.snippet.thumbnails.medium.url}
+									alt={video.snippet.title}
+								/>
+							</div>
+							<div className='desc'>
+								<p>{idx.length < 2 ? idx.padStart(2, '0') : idx}</p>
+								<span>{video.snippet.publishedAt.split('T')[0]}</span>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
