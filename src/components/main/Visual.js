3c46926054ef9bbfe626f9ac2c359153ebd9aadf
chahyunwoo <chahyunwoobi@gmail.com>
2022-05-17 16:34:44 +0900

    메인 페이지 수정

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -5,14 +5,18 @@ const path = process.env.PUBLIC_URL;
 function Visual() {
 	const frame = useRef(null);
 
-  useEffect(() => {
-    frame.current.classList.remove('on');
+	useEffect(() => {
+		frame.current.classList.remove('on');
 		frame.current.classList.add('on');
-  }, [])
+	}, []);
 
 	return (
 		<figure ref={frame}>
 			<img src={`${path}/images/bg.jpg`} alt='' />
+			<div className='scrollEvent'>
+				<div className='mouse'></div>
+				<p className='scroll'>Scroll</p>
+			</div>
 		</figure>
 	);
 }
